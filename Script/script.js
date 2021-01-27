2e91fb50ad6f3f4412942100167b2a5f80a56ab5
Mikey P <peraram2@gmail.com>
2021-01-27 12:28:18 -0800

    adding notes to clarify onclick function

--- Script/script.js
+++ Script/script.js
@@ -15,24 +15,26 @@ $(document).ready(function() {
   
   //Functions
       
- //give my button an ID
- //I retrieve that ID and it matches a class in my form and my div
- //when I run function the ID is pasted to this function so everything matches
-    
-       // When a .btn is clicked...
+     
+       // When a .btn is clicked this function takes in the ID from that button
+       //That ID matches the syntax for my form and final location to append
        $(".btn").on("click", function(){
         console.log(this.id, "this id")
+
         var appendAppt = $("<div>");
+        //I retrieve the ID from which button was clicked
         var correspondingElm = this.id;
+        //concatinate the button ID to match the cooresponding form ID
         var correspondingForm = $("#write-" + correspondingElm).val().trim();
                 
-        //add content(user input from form)
+        //add text from the form to the div that appends
         appendAppt.text(correspondingForm)
-        //choose location w/ variable (it changes by button)
+        //concatinate the Id name to make it a class, 
+        //where the div will append, i.e. ".9AM-appointment, .10AM-appointment"
         $("." + correspondingElm).append(appendAppt)
         //append
         console.log("button click")
-        
+        console.log(correspondingForm, "-text from form")
       
       });
 
